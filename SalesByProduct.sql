3740544c85b63ded168d452142939e7b12a2718b
NydiaGE <ge_nydi@hotmail.com>
2021-09-18 16:32:42 -0700

    Added OrderQuantity

--- SalesByProduct.sql
+++ SalesByProduct.sql
@@ -1,6 +1,7 @@
 SELECT
-            s.ProductKey        as ProductKey
-            ,sum(s.SalesAmount) as SalesAmount
+            s.ProductKey            as ProductKey
+            ,sum(s.SalesAmount)     as SalesAmount
+            ,sum(s.OrderQuantity)   as OrderQuantity
 FROM        dbo.FactInternetSales s
 LEFT JOIN   dbo.DimProduct p
     ON      s.ProductKey = p.ProductKey
